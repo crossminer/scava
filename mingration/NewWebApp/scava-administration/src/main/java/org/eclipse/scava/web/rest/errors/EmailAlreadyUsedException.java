53233cab21b966ee2c1fac5efb2280a0acddd456
aboudeffa <aboudeffa@FRP02422.softeam.com>
2018-04-20 16:09:21 +0200

    Revert "Initial application generated by JHipster-4.13.3"
    This reverts commit ee1cb3de1679d60f3f1b498b0816fa1ece95eae9.

--- mingration/NewWebApp/scava-administration/src/main/java/org/eclipse/scava/web/rest/errors/EmailAlreadyUsedException.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.scava.web.rest.errors;
-
-public class EmailAlreadyUsedException extends BadRequestAlertException {
-
-    public EmailAlreadyUsedException() {
-        super(ErrorConstants.EMAIL_ALREADY_USED_TYPE, "Email address already in use", "userManagement", "emailexists");
-    }
-}
