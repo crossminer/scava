6a29ccd6ab50ac007c84a0a8c66c6689fe281034
Lina Ochoa <lina.m8a@gmail.com>
2018-04-23 21:29:58 +0200

    [CWI] Finishing regression test + dump files (4 repos).

--- metric-platform/tests/org.eclipse.scava.rascal.test/src/org/eclipse/scava/rascal/test/regression/RascalMetricsTest.java
+++ metric-platform/tests/org.eclipse.scava.rascal.test/src/org/eclipse/scava/rascal/test/regression/RascalMetricsTest.java
@@ -4,17 +4,11 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.text.ParseException;
-import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 
-import org.bson.BSON;
 import org.bson.BSONDecoder;
 import org.bson.BSONObject;
 import org.bson.BasicBSONDecoder;
@@ -35,9 +29,6 @@ import com.googlecode.pongo.runtime.osgi.OsgiPongoFactoryContributor;
 import com.mongodb.Mongo;
 import com.mongodb.util.JSON;
 
-import sun.misc.IOUtils;
-
-import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
@@ -49,7 +40,6 @@ public class RascalMetricsTest {
 	private static final String NAME_SUFFIX = "_name";
 	private static final String URL_SUFFIX = "_url";
 	private static final String TEST_SUFFIX = "test_suffix";
-	private static final String[] METRIC_EXCEPTIONS = {"cocomo"};
 	
 	private static Platform platform;
 	private static Mongo mongo;
@@ -61,7 +51,7 @@ public class RascalMetricsTest {
 		PongoFactory.getInstance().getContributors().add(new OsgiPongoFactoryContributor());
 		platform = new Platform(mongo);
 
-		// Reading test properties
+		//Reading test properties
 		Properties prop = new Properties();
 		prop.load(this.getClass().getResourceAsStream("/config/local.properties"));
 
@@ -87,9 +77,9 @@ public class RascalMetricsTest {
 
 		System.out.println("Executing test project " + project.getName());
 		ProjectExecutor executor = new ProjectExecutor(platform, project);
-		// Compute metrics only for 9 dates
+		//Compute metrics only for 9 dates
 		Date dStart = executor.getLastExecutedDate();
-		Date dEnd = (new Date(dStart.toString())).addDays(1);
+		Date dEnd = (new Date(dStart.toString())).addDays(10);
 		executor.executeProject(dStart, dEnd);
 	}
 
@@ -97,7 +87,7 @@ public class RascalMetricsTest {
 	public void tearDown() throws Exception {
 		for(String db : dbs) {
 			System.out.println("Dropping database " + db);
-			//mongo.dropDatabase(db);
+			mongo.dropDatabase(db);
 		}
 		mongo.close();
 	}
@@ -114,94 +104,21 @@ public class RascalMetricsTest {
 			for(File f : files) {
 				String name = f.getName();
 				String col = name.substring(0, name.lastIndexOf("."));
-
-				JSONArray a1 = getJSONArrayFromDump(f);
-				JSONArray a2 = getJSONArrayFromDB(db, col);
-				result &= compareJSONArrays(a1, a2);
-
-				if(!result) {
-					System.err.println("There are different values in the " + db 
-							+ " database and the " + col + " collection.");
-					break;
-				}
-			}
-
-		}
-
-		assertTrue(result);
-	}
-
-
-	private boolean compareJSONArrays(JSONArray a1, JSONArray a2) {
-		// 1. Check number of objects in array
-		if(a1.length() != a2.length()) {
-			System.err.println("[ERROR] The dump file has " + a1.length() 
-			+ " objects while the new DB collection has " + a2.length() + " objects.");
-			return false;
-		}
-
-		// 2. Check that objects have the same value
-		for(int i = 0; i < a1.length(); i++) {
-			try {
-				boolean found = false;
-				Object obj = a1.get(i);
 				
-				if(obj instanceof JSONObject) {
-					JSONObject o1 = a1.getJSONObject(i);
+				//Only check Rascal collections.
+				if(col.contains("rascal")) {
+					JSONArray a1 = getJSONArrayFromDump(f);
+					JSONArray a2 = getJSONArrayFromDB(db, col);
+					result &= compareJSONArrays(a1, a2);
 
-					for(int j = 0; j < a2.length(); j++) {
-						
-						JSONObject o2 = a2.getJSONObject(j);
-						if(compareJSONObjects(o1,o2)) {
-							found = true;
-							// So we don't iterate multiple times over the same object.
-							a2.remove(j);
-							break;
-						}
+					if(!result) {
+						assertTrue("There are different values in the " + db 
+								+ " database and the " + col + " collection.", result);
 					}
-					if(!found) {
-						System.err.println("[ERROR] There is no object in the DB matching: ");
-						System.err.println(o1.toString());
-						return false;
-					}
-				}
-			}
-			catch(JSONException e) {
-				System.err.println("We got an error when getting a JSONObject: " + a1);
-			}
-		}
-
-		//3. Otherwise, everything is fine!
-		return true;
-	}
-
-	private boolean compareJSONObjects(JSONObject o1, JSONObject o2) {
-		String[] names = JSONObject.getNames(o1);
-		for(String name : names) {
-			//The _id/$id attribute is always different for different objects.
-			if(!name.equalsIgnoreCase("_id") && !name.equalsIgnoreCase("$id")) {
-				Object v1 = o1.get(name);
-				Object v2 = o2.get(name);
-
-				//Manage exceptions 
-				if(name.equalsIgnoreCase("metricId")) {
-					for(String e : METRIC_EXCEPTIONS) {
-						if(e.equalsIgnoreCase(v1.toString())) {
-							return true;
-						}
-					}
-				}
-				//Check if it is an array
-				if(v1 instanceof JSONArray && !compareJSONArrays((JSONArray) v1, (JSONArray) v2)) {
-					return false;
-				}
-				//And if it is not..
-				if(!(v1 instanceof JSONArray) && !v1.toString().equals(v2.toString())) {
-					return false;
 				}
 			}
 		}
-		return true;
+		assertTrue(result);
 	}
 
 	private JSONArray getJSONArrayFromDump(File dump) {
@@ -246,5 +163,69 @@ public class RascalMetricsTest {
 			System.out.println("We got an error when creating a JSONArray: " + e);
 			return null;
 		}
+	}
+	
+	private boolean compareJSONArrays(JSONArray a1, JSONArray a2) {
+		//1. Check number of objects in array
+		if(a1.length() != a2.length()) {
+			System.err.println("[ERROR] The dump file has " + a1.length() 
+			+ " objects while the new DB collection has " + a2.length() + " objects.");
+			return false;
+		}
+
+		//2. Check that objects have the same value
+		for(int i = 0; i < a1.length(); i++) {
+			try {
+				boolean found = false;
+				Object obj = a1.get(i);
+				
+				if(obj instanceof JSONObject) {
+					JSONObject o1 = a1.getJSONObject(i);
+
+					for(int j = 0; j < a2.length(); j++) {
+						JSONObject o2 = a2.getJSONObject(j);
+						if(compareJSONObjects(o1,o2)) {
+							found = true;
+							// So we don't iterate multiple times over the same object.
+							a2.remove(j);
+							break;
+						}
+					}
+					if(!found) {
+						return false;
+					}
+				}
+			}
+			catch(JSONException e) {
+				System.err.println("We got an error when getting a JSONObject: " + a1);
+			}
+		}
+
+		//3. Otherwise, everything is fine!
+		return true;
+	}
+
+	private boolean compareJSONObjects(JSONObject o1, JSONObject o2) {
+		String[] names = JSONObject.getNames(o1);
+		for(String name : names) {
+			if(!o2.has(name)) {
+				return false;
+			}
+			//The _id/$id attribute is always different for different objects.
+			if(!name.equalsIgnoreCase("_id") && !name.equalsIgnoreCase("$id")) {
+				Object v1 = o1.get(name);
+				Object v2 = o2.get(name);
+
+				//Check if it is an array
+				if(v1 instanceof JSONArray && !compareJSONArrays((JSONArray) v1, (JSONArray) v2)) {
+					return false;
+				}
+				//And if it is not..
+				if(!(v1 instanceof JSONArray) && !v1.toString().equals(v2.toString())) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
