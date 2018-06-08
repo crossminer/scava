2789255fca25f0794e1552d30ebf7aa62c989114
Lina Ochoa <lina.m8a@gmail.com>
2018-06-08 15:31:35 +0200

    [CWI] Adding license to source files.

--- metric-platform/platform/org.eclipse.scava.dependency.model/org.eclipse.scava.dependency.model.maven/src/org/eclipse/scava/dependency/model/maven/util/internal/MavenVersionHelper.java
+++ metric-platform/platform/org.eclipse.scava.dependency.model/org.eclipse.scava.dependency.model.maven/src/org/eclipse/scava/dependency/model/maven/util/internal/MavenVersionHelper.java
@@ -1,3 +1,12 @@
+/*********************************************************************
+* Copyright (c) 2018 Centrum Wiskunde & Informatica
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
 package org.eclipse.scava.dependency.model.maven.util.internal;
 
 import org.apache.maven.artifact.versioning.ComparableVersion;
