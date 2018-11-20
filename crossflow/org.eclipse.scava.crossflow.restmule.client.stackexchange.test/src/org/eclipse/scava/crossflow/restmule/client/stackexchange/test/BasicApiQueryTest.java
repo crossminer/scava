3648afac9332d11adb2e61f1f36542bd2469646d
Patrick Neubauer <patrick.neubauer@york.ac.uk>
2018-11-20 19:03:29 +0000

    updates tests for restmule stackexchange client

--- crossflow/org.eclipse.scava.crossflow.restmule.client.stackexchange.test/src/org/eclipse/scava/crossflow/restmule/client/stackexchange/test/BasicApiQueryTest.java
+++ crossflow/org.eclipse.scava.crossflow.restmule.client.stackexchange.test/src/org/eclipse/scava/crossflow/restmule/client/stackexchange/test/BasicApiQueryTest.java
@@ -13,6 +13,7 @@ import org.eclipse.scava.crossflow.restmule.client.stackexchange.api.IStackExcha
 import org.eclipse.scava.crossflow.restmule.client.stackexchange.util.StackExchangeUtils;
 import org.eclipse.scava.crossflow.restmule.core.data.IData;
 import org.eclipse.scava.crossflow.restmule.core.data.IDataSet;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BasicApiQueryTest extends StackExchangeUtils {
@@ -22,7 +23,7 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 
 	
 	@Test
-	public void testPublicSessionTotal() {
+	public void testPublicSessionSmall() {
 		// Instantiate API client
 		IStackExchangeApi client = StackExchangeUtils.getOAuthClient();
 		
@@ -33,9 +34,7 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		String sort = "creation";
 		Integer fromdate = 1541091600;
 		Integer todate = 1541092200; // 10 results
-		Integer pagesize = 4;
-		Integer page = 1;
-		String filter = "total"; // the "total" filter will be removed after the 1st call (it is used to determine the total number of results required for pagination)
+		String filter = null;
 		String callback = null;//"";
 		String site = "stackoverflow";
 	
@@ -46,49 +45,7 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		result.observe()
 		
 			.doOnNext(resultItem -> {
-				LOG.info(result.hashCode() + " --- " + resultItem.toString());
-			})
-			
-			.doOnError(e -> {
-				LOG.error(result.hashCode() + " --- " + "ERROR OCCURRED: " + e.getMessage());
-			})
-			
-			.doOnComplete(() -> {
-				LOG.info(result.hashCode() + " --- " + "COMPLETED !");
-			})
-			
-			.blockingSubscribe();
-	
-		Long count = result.observe().count().blockingGet();
-		LOG.info("Final observe count of task " + result.hashCode() + ": " + count);
-	}
-	
-	@Test
-	public void testPublicSessionTotalLarge() {
-		// Instantiate API client
-		IStackExchangeApi client = StackExchangeUtils.getOAuthClient();
-		
-		String tagged = "java";
-		String order = "desc";
-		String max = null;//"";
-		String min = null;//"";
-		String sort = "creation";
-		Integer fromdate = 1541091600;
-		Integer todate = 1541113200; // 11/01/2018 @ 11:00pm (UTC) --- more than 100 results
-		Integer pagesize = 4;
-		Integer page = 1;
-		String filter = "total"; // the "total" filter will be removed after the 1st call (it is used to determine the total number of results required for pagination)
-		String callback = null;//"";
-		String site = "stackoverflow";
-	
-		// Submit API query
-		IData<QuestionsResponse> result = client.getQuestionsQuestionsResponse(tagged, order, max, min, sort, fromdate, todate, filter, callback, site);
-		
-		// Observe search repositories result set
-		result.observe()
-		
-			.doOnNext(resultItem -> {
-				LOG.info(result.hashCode() + " --- " + resultItem.toString());
+				LOG.info(result.hashCode() + " --- ITEM: " + resultItem.toString());
 			})
 			
 			.doOnError(e -> {
@@ -117,8 +74,6 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		String sort = "creation";
 		Integer fromdate = 1541091600; // 11/01/2018 @ 5:00pm (UTC)
 		Integer todate = 1541095200; // 11/01/2018 @ 6:00pm (UTC) --- 33 results
-		Integer pagesize = 10;
-		Integer page = 1;
 		String filter = null;//"";
 		String callback = null;//""
 		String site = "stackoverflow";
@@ -131,8 +86,7 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		result.observe()
 		
 			.doOnNext(resultItem -> {
-				System.out.println("doOnNext.result="+resultItem.toString());
-//				LOG.info(questions.id() + " --- " + result.getQuestionId());
+				LOG.info(result.hashCode() + " --- ITEM: " + resultItem.toString());
 			})
 			
 			.doOnError(e -> {
@@ -145,13 +99,6 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 			
 			.blockingSubscribe();
 
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
 		Long count = result.observe().count().blockingGet();
 		LOG.info("Final observe count of task " + result.hashCode() + ": " + count);
 
@@ -168,9 +115,7 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		String min = null;//"";
 		String sort = "creation";
 		Integer fromdate = 1541091600; // 11/01/2018 @ 5:00pm (UTC)
-		Integer todate = 1541113200; // 11/01/2018 @ 11:00pm (UTC) --- more than 100 results
-		Integer pagesize = 10;
-		Integer page = 1;
+		Integer todate = 1541113200; // 11/01/2018 @ 11:00pm (UTC) --- ~138 results
 		String filter = null;//"";
 		String callback = null;//""
 		String site = "stackoverflow";
@@ -183,8 +128,7 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		result.observe()
 		
 			.doOnNext(resultItem -> {
-				System.out.println("doOnNext.result="+resultItem.toString());
-//				LOG.info(questions.id() + " --- " + result.getQuestionId());
+				LOG.info(result.hashCode() + " --- ITEM: " + resultItem.toString());
 			})
 			
 			.doOnError(e -> {
@@ -197,13 +141,6 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 			
 			.blockingSubscribe();
 
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
 		Long count = result.observe().count().blockingGet();
 		LOG.info("Final observe count of task " + result.hashCode() + ": " + count);
 
@@ -221,8 +158,6 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		String sort = "creation";
 		Integer fromdate = 1541091600; // 11/01/2018 @ 5:00pm (UTC)
 		Integer todate = 1541095200; // 11/01/2018 @ 6:00pm (UTC) --- 33 results
-		Integer pagesize = 10;
-		Integer page = 1;
 		String filter = null;//"";
 		String callback = null;//""
 		String site = "stackoverflow";
@@ -235,7 +170,7 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		result.observe()
 		
 			.doOnNext(resultItem -> {
-				LOG.info(result.hashCode() + " --- " + resultItem.toString());
+				LOG.info(result.hashCode() + " --- ITEM: " + resultItem.toString());
 			})
 			
 			.doOnError(e -> {
@@ -248,13 +183,6 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 			
 			.blockingSubscribe();
 
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
 		Long count = result.observe().count().blockingGet();
 		LOG.info("Final observe count of task " + result.hashCode() + ": " + count);
 
@@ -273,8 +201,6 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		String sort = "popular";
 		Integer fromdate = 1535760000;
 		Integer todate = 1542412800;
-		Integer pagesize = 10;
-		Integer page = 1;
 		String filter = null;//"";
 		String callback = null;//""
 		String site = "stackoverflow";
@@ -286,7 +212,7 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		result.observe()
 		
 			.doOnNext(resultItem -> {
-				LOG.info(result.hashCode() + " --- " + resultItem.toString());
+				LOG.info(result.hashCode() + " --- ITEM: " + resultItem.toString());
 			})
 			
 			.doOnError(e -> {
@@ -298,13 +224,6 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 			})
 			
 			.blockingSubscribe();
-
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
 		
 		Long count = result.observe().count().blockingGet();
 		LOG.info("Final observe count of task " + result.hashCode() + ": " + count);
@@ -324,8 +243,6 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		String sort = "reputation";
 		Integer fromdate = 1535760000;
 		Integer todate = 1541095200;
-		Integer pagesize = 10;
-		Integer page = 1;
 		String filter = null;//"";
 		String callback = null;//""
 		String site = "stackoverflow";
@@ -337,7 +254,7 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		result.observe()
 		
 			.doOnNext(resultItem -> {
-				LOG.info(result.hashCode() + " --- " + resultItem.toString());
+				LOG.info(result.hashCode() + " --- ITEM: " + resultItem.toString());
 			})
 			
 			.doOnError(e -> {
@@ -349,13 +266,6 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 			})
 			
 			.blockingSubscribe();
-
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
 		
 		Long count = result.observe().count().blockingGet();
 		LOG.info("Final observe count of task " + result.hashCode() + ": " + count);
@@ -374,8 +284,6 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		String sort = "votes";
 		Integer fromdate = 1535760000;
 		Integer todate = 1541095200;
-		Integer pagesize = 10;
-		Integer page = 1;
 		String filter = null;//"";
 		String callback = null;//""
 		String site = "stackoverflow";
@@ -387,7 +295,7 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		result.observe()
 		
 			.doOnNext(resultItem -> {
-				LOG.info(result.hashCode() + " --- " + resultItem.toString());
+				LOG.info(result.hashCode() + " --- ITEM: " + resultItem.toString());
 			})
 			
 			.doOnError(e -> {
@@ -399,13 +307,6 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 			})
 			
 			.blockingSubscribe();
-
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
 		
 		Long count = result.observe().count().blockingGet();
 		LOG.info("Final observe count of task " + result.hashCode() + ": " + count);
@@ -422,10 +323,8 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		String min = null;//"";
 		String sort = "creation";
 		Integer fromdate = 1541091600;
-		Integer todate = 1541712821;// (now) //1541092200; // 10 results
-		Integer pagesize = 5; // (many) // 2 pages
-		Integer page = 1;
-		String filter = null;//"";
+		Integer todate = 1541113200; // 11/01/2018 @ 11:00pm (UTC) --- more than 100 results
+		String filter = "total";//"total";
 		String callback = null;//""
 		String site = "stackoverflow";
 	
@@ -436,7 +335,7 @@ public class BasicApiQueryTest extends StackExchangeUtils {
 		result.observe()
 		
 			.doOnNext(resultItem -> {
-				LOG.info(result.hashCode() + " --- " + resultItem.toString());
+				LOG.info(result.hashCode() + " --- ITEM: " + resultItem.toString());
 			})
 			
 			.doOnError(e -> {
