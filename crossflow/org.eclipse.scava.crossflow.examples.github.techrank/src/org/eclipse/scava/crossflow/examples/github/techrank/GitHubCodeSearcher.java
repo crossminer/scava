7278d8bef2e25d326c511db77eb3d197101c4b7f
dkolovos <dkolovos@cs.york.ac.uk>
2018-12-20 20:05:00 +0000

    Further developed techrank example

--- crossflow/org.eclipse.scava.crossflow.examples.github.techrank/src/org/eclipse/scava/crossflow/examples/github/techrank/GitHubCodeSearcher.java
+++ crossflow/org.eclipse.scava.crossflow.examples.github.techrank/src/org/eclipse/scava/crossflow/examples/github/techrank/GitHubCodeSearcher.java
@@ -1,19 +1,36 @@
 package org.eclipse.scava.crossflow.examples.github.techrank;
 
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
+import java.util.Properties;
+
+import org.kohsuke.github.GHContent;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+import org.kohsuke.github.PagedSearchIterable;
+import org.kohsuke.github.extras.OkHttpConnector;
+
+import com.squareup.okhttp.Cache;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.OkUrlFactory;
 
 public class GitHubCodeSearcher extends GitHubCodeSearcherBase {
 	
+	protected GitHub github = null;
+	
 	@Override
 	public void consumeTechnologies(Technology technology) throws Exception {
+		if (github == null) connectToGitHub();
+
+		PagedSearchIterable<GHContent> it = github.searchContent().extension(technology.getExtension()).q(technology.getKeyword()).list();
+		it.forEach(c -> sendToRepositories(new Repository(c.getOwner().getFullName(), technology)));
+	}
+	
+	protected void connectToGitHub() throws Exception {		
+		Properties properties = new TechrankWorkflowContext(workflow).getProperties();
 		
-		List<String> repositories = Arrays.asList("hellspawn14/WebPicture-Samples", "Mastrognu/BramboProject");
-		
-		for (String repository : repositories) {
-			sendToRepositories(new Repository(repository, technology));
-		}
-		
+		Cache cache = new Cache(new File("okhttp-cache"), 10 * 1024 * 1024); // 100MB cache
+		github = GitHubBuilder.fromProperties(properties).
+			withConnector(new OkHttpConnector(new OkUrlFactory(new OkHttpClient().setCache(cache)))).build();
 	}
 
 }
--- crossflow/org.eclipse.scava.crossflow.examples.github.techrank/src/org/eclipse/scava/crossflow/examples/github/techrank/TechrankApp.java
+++ crossflow/org.eclipse.scava.crossflow.examples.github.techrank/src/org/eclipse/scava/crossflow/examples/github/techrank/TechrankApp.java
@@ -1,22 +1,29 @@
 package org.eclipse.scava.crossflow.examples.github.techrank;
 
-import org.kohsuke.github.GHContent;
-import org.kohsuke.github.GitHub;
-import org.kohsuke.github.PagedSearchIterable;
+import org.eclipse.scava.crossflow.runtime.FailedJob;
+import org.eclipse.scava.crossflow.runtime.InternalException;
 
 public class TechrankApp {
 	
 	public static void main(String[] args) throws Exception {
 		
-		/*
 		TechrankWorkflow workflow = new TechrankWorkflow();
+		//workflow.setCache(new DirectoryCache(new File("cache")));
 		workflow.run();
 		
 		while (!workflow.hasTerminated()) {
 			Thread.sleep(100);
 		}
 		
-		System.exit(0);*/
+		for (InternalException ex : workflow.getInternalExceptions()) {
+			ex.getException().printStackTrace();
+		}
+		
+		for (FailedJob failed : workflow.getFailedJobs()) {
+			failed.getException().printStackTrace();
+		}
+		
+		System.exit(0);
 	}
 	
 }
