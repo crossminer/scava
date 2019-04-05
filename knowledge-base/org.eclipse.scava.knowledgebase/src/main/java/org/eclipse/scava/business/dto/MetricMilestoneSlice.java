d3e76c905172aac613dbe17d5958c1bc70bb1225
md2manoppello <juri.di.rocco@gmail.com>
2019-04-05 17:00:34 +0200

    #Issue #121. POST http://localhost:8080/api/artifacts/store-metrics

---
+++ knowledge-base/org.eclipse.scava.knowledgebase/src/main/java/org/eclipse/scava/business/dto/MetricMilestoneSlice.java
@@ -0,0 +1,22 @@
+package org.eclipse.scava.business.dto;
+
+import java.util.List;
+
+public class MetricMilestoneSlice {
+
+	private String bounder;
+	private List<MetricBoundary> boundary;
+	public String getBounder() {
+		return bounder;
+	}
+	public void setBounder(String bounder) {
+		this.bounder = bounder;
+	}
+	public List<MetricBoundary> getBoundary() {
+		return boundary;
+	}
+	public void setBoundary(List<MetricBoundary> boundary) {
+		this.boundary = boundary;
+	}
+
+}
