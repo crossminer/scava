b5505d484b11fbfbe9f9e1c0c131954acc4ddc4c
tdegueul <degueule@cwi.nl>
2019-04-19 17:52:36 +0200

    Remove non-alphanumeric characters in project's shortName to avoid rejected/duplicated MongoDB collections

--- metric-platform/platform/org.eclipse.scava.platform.client.api/src/org/eclipse/scava/platform/client/api/ProjectImporter.java
+++ metric-platform/platform/org.eclipse.scava.platform.client.api/src/org/eclipse/scava/platform/client/api/ProjectImporter.java
@@ -104,6 +104,10 @@ public class ProjectImporter {
 			}
 		}
 		
+		// Post-processing: remove any special character in the project's name
+		// to avoid possible conflicts, e.g. with MongoDB collection name restrictions
+		p.setShortName(p.getShortName().replaceAll("[^A-Za-z0-9]", ""));
+
 		return p;
 	}
 
