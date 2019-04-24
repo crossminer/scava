77c396eb5f92ad923804205fea8529c16d0135d9
ambpro <amin.boudeffa@softeam.fr>
2019-04-24 14:25:15 +0200

    Fix filtering metrics selection #164

--- administration/scava-administration/src/app/layout/project/components/configure-project/analysis-task-add/analysis-task-add.component.ts
+++ administration/scava-administration/src/app/layout/project/components/configure-project/analysis-task-add/analysis-task-add.component.ts
@@ -115,7 +115,8 @@ export class AnalysisTaskAddComponent implements OnInit {
   masterToggle() {
     this.isAllSelected() ?
       this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
+      this.dataSource.filteredData.forEach(row => this.selection.select(row));
+      console.log(this.dataSource.filteredData)
   }
 
   onRowSelect(row) {
