0592ba78ca6289d1fa8c18468cb3526a32e1d722
D20 <danielcampbell2097@hotmail.com>
2019-05-08 15:29:18 +0100

    removed dependency for CommunicationChannel Posts & Forums

--- metric-platform/platform-extensions/org.eclipse.scava.platform.communicationchannel.eclipseforums/src/org/eclipse/scava/platform/communicationchannel/eclipseforums/EclipseForumsTopic.java
+++ metric-platform/platform-extensions/org.eclipse.scava.platform.communicationchannel.eclipseforums/src/org/eclipse/scava/platform/communicationchannel/eclipseforums/EclipseForumsTopic.java
@@ -9,17 +9,16 @@
  ******************************************************************************/
 package org.eclipse.scava.platform.communicationchannel.eclipseforums;
 
-
 import java.util.Date;
 
-import org.eclipse.scava.platform.delta.communicationchannel.CommunicationChannelTopic;
-
-
-public class EclipseForumsTopic extends CommunicationChannelTopic{
+public class EclipseForumsTopic{
 	
-	private static final long serialVersionUID = 1L;
-
-
+	private String forum_id;
+	private String topic_id;
+	private String name;
+	private String description;
+	private String url;
+	private Date creation_date;
 	private String topic_subject;
 	private String topic_html_url;
 	private String root_post_id;
@@ -31,7 +30,46 @@ public class EclipseForumsTopic extends CommunicationChannelTopic{
 	private Date last_post_date;
 	
 	
-
+	public String getForum_id() {
+		return forum_id;
+	}
+	public void setForum_id(String forum_id) {
+		this.forum_id = forum_id;
+	}
+	public String getTopic_id() {
+		return topic_id;
+	}
+	public void setTopic_id(String topic_id) {
+		this.topic_id = topic_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public Date getCreation_date() {
+		return creation_date;
+	}
+	public void setCreation_date(Date creation_date) {
+		this.creation_date = creation_date;
+	}
+	public String getTopic_subject() {
+		return topic_subject;
+	}
+	
 	/**
 	 * @return the first_post_date
 	 */
