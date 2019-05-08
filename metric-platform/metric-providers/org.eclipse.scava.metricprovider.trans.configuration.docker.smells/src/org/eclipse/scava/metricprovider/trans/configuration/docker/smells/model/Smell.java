7156beb18b3718c5c8fd0be76f62cf5cbc5fd986
Konstantinos Kravvaritis <blueoly@gmail.com>
2019-05-08 18:50:45 +0300

    add 'line' field in docker Smell class

--- metric-platform/metric-providers/org.eclipse.scava.metricprovider.trans.configuration.docker.smells/src/org/eclipse/scava/metricprovider/trans/configuration/docker/smells/model/Smell.java
+++ metric-platform/metric-providers/org.eclipse.scava.metricprovider.trans.configuration.docker.smells/src/org/eclipse/scava/metricprovider/trans/configuration/docker/smells/model/Smell.java
@@ -10,11 +10,13 @@ public class Smell extends Pongo {
 		SMELLNAME.setOwningType("org.eclipse.scava.metricprovider.trans.configuration.docker.smells.model.Smell");
 		REASON.setOwningType("org.eclipse.scava.metricprovider.trans.configuration.docker.smells.model.Smell");
 		FILENAME.setOwningType("org.eclipse.scava.metricprovider.trans.configuration.docker.smells.model.Smell");
+		LINE.setOwningType("org.eclipse.scava.metricprovider.trans.configuration.docker.smells.model.Smell");
 	}
 	
 	public static StringQueryProducer SMELLNAME = new StringQueryProducer("smellName"); 
 	public static StringQueryProducer REASON = new StringQueryProducer("reason");
-	public static StringQueryProducer FILENAME = new StringQueryProducer("fileName"); 
+	public static StringQueryProducer FILENAME = new StringQueryProducer("fileName");
+	public static StringQueryProducer LINE = new StringQueryProducer("line");
 	
 	
 	
@@ -45,6 +47,16 @@ public class Smell extends Pongo {
 	
 	public Smell setFileName(String fileName) {
 		dbObject.put("fileName", fileName);
+		notifyChanged();
+		return this;
+	}
+	
+	public String getLine() {
+		return parseString(dbObject.get("line")+"", "");
+	}
+	
+	public Smell setLine(String line) {
+		dbObject.put("line", line);
 		notifyChanged();
 		return this;
 	}
