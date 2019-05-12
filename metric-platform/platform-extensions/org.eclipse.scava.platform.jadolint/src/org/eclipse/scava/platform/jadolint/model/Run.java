ae455940fc1dbc79f25aa3086c8b86e0d25d15d7
Konstantinos Kravvaritis <blueoly@gmail.com>
2019-05-13 01:52:54 +0300

    change Run and RunBlock classes to detect sudo command as part of the executable

--- metric-platform/platform-extensions/org.eclipse.scava.platform.jadolint/src/org/eclipse/scava/platform/jadolint/model/Run.java
+++ metric-platform/platform-extensions/org.eclipse.scava.platform.jadolint/src/org/eclipse/scava/platform/jadolint/model/Run.java
@@ -81,7 +81,7 @@ public class Run implements Instruction{
         
         for(RunBlock rb : runBlocks){
             String exec = rb.getExecutable();
-            if(exec.equals("apt-get")){
+            if(exec.equals("sudo apt-get")){
                 String params = rb.getParams();
                 
                 String[] paramsArray = params.split(" ");
@@ -102,7 +102,7 @@ public class Run implements Instruction{
         
         for(RunBlock rb : runBlocks){
             String exec = rb.getExecutable();
-            if(exec.equals("pip")){
+            if(exec.equals("sudo pip")){
                 String params = rb.getParams();
                 
                 String[] paramsArray = params.split(" ");
@@ -123,7 +123,7 @@ public class Run implements Instruction{
         
         for(RunBlock rb : runBlocks){
             String exec = rb.getExecutable();
-            if(exec.equals("apk")){
+            if(exec.equals("sudo apk")){
                 String params = rb.getParams();
                 
                 String[] paramsArray = params.split(" ");
