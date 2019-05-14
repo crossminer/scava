e1ba77b19eb0c30fe05b5e71d85d99c9457d2de2
Luis Adrian Cabrera Diego <adrian89_@hotmail.com>
2019-05-14 10:17:07 +0100

    Support for Markdown documentation GitHub

--- metric-platform/metric-providers/org.eclipse.scava.metricprovider.trans.documentation/src/org/eclipse/scava/metricprovider/trans/documentation/DocumentationTransMetricProvider.java
+++ metric-platform/metric-providers/org.eclipse.scava.metricprovider.trans.documentation/src/org/eclipse/scava/metricprovider/trans/documentation/DocumentationTransMetricProvider.java
@@ -2,25 +2,32 @@ package org.eclipse.scava.metricprovider.trans.documentation;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
+import java.util.Set;
 
+import org.eclipse.scava.metricprovider.trans.documentation.model.DocumentationEntry;
 import org.eclipse.scava.metricprovider.trans.documentation.model.DocumentationTransMetric;
-import com.mongodb.DB;
-
-import org.eclipse.scava.platform.Constants;
+import org.eclipse.scava.nlp.tools.plaintext.PlainTextObject;
+import org.eclipse.scava.nlp.tools.plaintext.documentation.PlainTextDocumentationMarkdownBased;
+import org.eclipse.scava.nlp.tools.preprocessor.fileparser.FileParser;
 import org.eclipse.scava.platform.IMetricProvider;
 import org.eclipse.scava.platform.ITransientMetricProvider;
 import org.eclipse.scava.platform.MetricProviderContext;
 import org.eclipse.scava.platform.delta.ProjectDelta;
 import org.eclipse.scava.platform.delta.vcs.PlatformVcsManager;
+import org.eclipse.scava.platform.delta.vcs.VcsChangeType;
 import org.eclipse.scava.platform.delta.vcs.VcsCommit;
 import org.eclipse.scava.platform.delta.vcs.VcsCommitItem;
 import org.eclipse.scava.platform.delta.vcs.VcsProjectDelta;
 import org.eclipse.scava.platform.delta.vcs.VcsRepositoryDelta;
+import org.eclipse.scava.platform.logging.OssmeterLogger;
 import org.eclipse.scava.platform.vcs.workingcopy.manager.WorkingCopyCheckoutException;
 import org.eclipse.scava.platform.vcs.workingcopy.manager.WorkingCopyFactory;
 import org.eclipse.scava.platform.vcs.workingcopy.manager.WorkingCopyManagerUnavailable;
@@ -28,9 +35,17 @@ import org.eclipse.scava.repository.model.Project;
 import org.eclipse.scava.repository.model.VcsRepository;
 import org.eclipse.scava.repository.model.github.GitHubWiki;
 
+import com.mongodb.DB;
+
 public class DocumentationTransMetricProvider implements ITransientMetricProvider<DocumentationTransMetric> {
 
 	protected PlatformVcsManager platformVcsManager;
+	
+	protected OssmeterLogger logger;
+	
+	public DocumentationTransMetricProvider() {
+		logger = (OssmeterLogger) OssmeterLogger.getLogger("metricprovider.trans.documentation");
+	}
 	
 	@Override
 	public String getIdentifier() {
@@ -39,20 +54,17 @@ public class DocumentationTransMetricProvider implements ITransientMetricProvide
 
 	@Override
 	public String getShortIdentifier() {
-		// TODO Auto-generated method stub
-		return null;
+		return "trans.documentation";
 	}
 
 	@Override
 	public String getFriendlyName() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Documentation processing";
 	}
 
 	@Override
 	public String getSummaryInformation() {
-		// TODO Auto-generated method stub
-		return null;
+		return "This metric process the files or text returned from the documentation readers and transform raw text into plain text.";
 	}
 
 	@Override
@@ -87,43 +99,99 @@ public class DocumentationTransMetricProvider implements ITransientMetricProvide
 	@Override
 	public void measure(Project project, ProjectDelta projectDelta, DocumentationTransMetric db) {
 
-
+		db.getDocumentationEntries().getDbCollection().drop();
 		
 		VcsProjectDelta vcsProjectDelta = projectDelta.getVcsDelta();
 		for(VcsRepositoryDelta vcsDelta : vcsProjectDelta.getRepoDeltas())
 		{
+			Set<String> filesToProcess = new HashSet<String>();
+			VcsRepository repository = vcsDelta.getRepository();
+			String lastRevision="";
+			
+			
 			for(VcsCommit commit : vcsDelta.getCommits())
 			{
-				String revision = commit.getRevision();
+				lastRevision = commit.getRevision();
+				for(VcsCommitItem item : commit.getItems())
+				{
+					if(item.getChangeType()==VcsChangeType.ADDED || item.getChangeType()==VcsChangeType.UPDATED)
+						filesToProcess.add(item.getPath());
+					if(item.getChangeType()==VcsChangeType.DELETED)
+						filesToProcess.removeIf(file -> file.equals(item.getPath()));
+				}
+			}
+			
+			if(filesToProcess.size()>0)
+			{
 				Map<String, File> workingCopyFolders = new HashMap<String, File>();				
 				Map<String, File> scratchFolders = new HashMap<String, File>();
 				
 				try {
-					WorkingCopyFactory.getInstance().checkout(project, revision, workingCopyFolders, scratchFolders);
+					WorkingCopyFactory.getInstance().checkout(project, lastRevision, workingCopyFolders, scratchFolders);
+					File file;
+					String fileContent;
+					for(String relativePath : filesToProcess)
+					{
+						try {
+								file = new File(workingCopyFolders.get(repository.getUrl())+"/"+relativePath);
+								if(!Files.exists(file.toPath()))
+									throw new FileNotFoundException("The file "+relativePath+" has not been found");
+								fileContent = FileParser.extractTextAsString(file);
+								DocumentationEntry documentationEntry = findDocumentationEntry(db, repository.getUrl(), relativePath);
+								if(documentationEntry==null)
+								{
+									documentationEntry = new DocumentationEntry();
+									documentationEntry.setDocumentationId(repository.getUrl());
+									documentationEntry.setEntryId(relativePath);
+								}
+								else
+									documentationEntry.getPlainText().clear();
+								documentationEntry.getPlainText().addAll(getPlainText(file.getName(), fileContent));
+								db.getDocumentationEntries().add(documentationEntry);
+						}
+						catch(InvalidPathException e)
+						{
+							logger.error("File: "+relativePath+" couldn't be read from the working copy. It contains an illegal character "
+									+ "i.e. \\ / : * ? \" < > |");
+						} catch (FileNotFoundException e) {
+							logger.error("File: "+relativePath+" couldn't be found in the working copy.", e);
+							e.printStackTrace();
+						}
+					}
+					db.sync();
+					
 				} catch (WorkingCopyManagerUnavailable | WorkingCopyCheckoutException e) {
-					// TODO Auto-generated catch block
+					logger.error("Error while creating local copy of documentation:", e);
 					e.printStackTrace();
 				}
-				for(VcsCommitItem item : commit.getItems())
-				{
-					File file = new File(workingCopyFolders.get(vcsDelta.getRepository().getUrl())+"/"+item.getPath());
-					String contents ="";
-					Scanner scanner;
-					try {
-						scanner = new Scanner(file);
-						while(scanner.hasNext()) {
-							contents += scanner.nextLine() + Constants.NEW_LINE; 
-						}
-						scanner.close();
-					} catch (FileNotFoundException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					
-				}
+				
 			}
+			
 		}
 		
+	}
+	
+	private List<String> getPlainText(String fileName, String fileContent)
+	{
+		PlainTextObject plainText=null;
+		if(fileName.endsWith(".md") || fileName.endsWith(".markdown"))
+		{
+			plainText = PlainTextDocumentationMarkdownBased.process(fileContent);
+		}
+		if(plainText==null)
+			return new ArrayList<String>(0);
+		return plainText.getPlainTextAsList();
+	}
+	
+	private DocumentationEntry findDocumentationEntry(DocumentationTransMetric db, String documentationId, String relativePathFile)
+	{
+		DocumentationEntry documentationEntry = null;
+		Iterable<DocumentationEntry> documentationEntryIt = db.getDocumentationEntries().
+				find(DocumentationEntry.DOCUMENTATIONID.eq(documentationId),
+						DocumentationEntry.ENTRYID.eq(relativePathFile));
+		for(DocumentationEntry de : documentationEntryIt)
+			documentationEntry=de;
+		return documentationEntry;
 	}
 
 }
--- metric-platform/metric-providers/org.eclipse.scava.nlp.tools.plaintext/src/org/eclipse/scava/nlp/tools/plaintext/bugtrackers/PlainTextBugTrackerMarkdownBased.java
+++ metric-platform/metric-providers/org.eclipse.scava.nlp.tools.plaintext/src/org/eclipse/scava/nlp/tools/plaintext/bugtrackers/PlainTextBugTrackerMarkdownBased.java
@@ -9,6 +9,8 @@
  ******************************************************************************/
 package org.eclipse.scava.nlp.tools.plaintext.bugtrackers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 import org.eclipse.scava.nlp.tools.plaintext.PlainTextObject;
@@ -17,22 +19,27 @@ import org.eclipse.scava.nlp.tools.plaintext.utils.ReplyLineDetection;
 import org.eclipse.scava.nlp.tools.preprocessor.htmlparser.HtmlParser;
 import org.eclipse.scava.nlp.tools.preprocessor.markdown.MarkdownParser;
 
-
-class MarkdownToPlainText
+public class PlainTextBugTrackerMarkdownBased
 {
+	private static Pattern newlines;
 	private static Pattern escapedNewline;
 	
 	static
 	{
+		newlines=Pattern.compile("\\\\r\\\\n");
 		escapedNewline = Pattern.compile("(\\\\n|\\\\r)");
 	}
 	
 	public static PlainTextObject process(String text)
 	{
+		if(text==null)
+			return new PlainTextObject(new ArrayList<String>(Arrays.asList("")));
+		text=newlines.matcher(text).replaceAll("\n");
 		IntermadiatePlainTextObject intermadiatePlainTextObject= ReplyLineDetection.process(text);
 		text= MarkdownParser.parse(intermadiatePlainTextObject.getPlainText());
 		//In case the text contain escaped newlines
 		text=escapedNewline.matcher(text).replaceAll("");
 		return new PlainTextObject(HtmlParser.parse(text), intermadiatePlainTextObject.hadReplies());
 	}
+
 }
