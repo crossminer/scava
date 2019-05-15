48b4a58cc2e93d4f8459db55830f80d50e117fa3
Patrick Neubauer <patrick.neubauer@york.ac.uk>
2019-05-15 12:29:29 +0100

    adapts o.e.s.c.examples.wordcount: single text file input

--- crossflow/org.eclipse.scava.crossflow.examples.wordcount/src/org/eclipse/scava/crossflow/examples/wordcount/LineSource.java
+++ crossflow/org.eclipse.scava.crossflow.examples.wordcount/src/org/eclipse/scava/crossflow/examples/wordcount/LineSource.java
@@ -11,14 +11,13 @@ public class LineSource extends LineSourceBase {
 	
 	@Override
 	public void produce() throws Exception {
-		for (File file : workflow.getInputDirectory().listFiles()) {
-			BufferedReader reader = new BufferedReader(new FileReader(file));
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				sendToLines(new Line(line));
-			}
-			reader.close();
+		File file = new File("a-tale-of-two-cities.txt").getAbsoluteFile();
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			sendToLines(new Line(line));
 		}
+		reader.close();
 	}
 
 }
