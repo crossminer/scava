a21353afbddfa24bf872be2ce4ae52c7f4b7f119
ambpro <amin.boudeffa@softeam.fr>
2019-05-16 10:56:05 +0200

    Remove old ossmeter webapp

--- migration/OldWebApp/org-ossmeter-webapp/app/com/googlecode/pongo/runtime/PongoFactoryContributor.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.pongo.runtime;
-
-public interface PongoFactoryContributor {
-	
-	public boolean canCreate(String className);
-	
-	public Pongo create(String className) throws Exception;
-	
-}
