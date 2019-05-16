795ddc85644e4f41530d6010b47fb547aeea4a74
D20 <danielcampbell2097@hotmail.com>
2019-05-16 10:28:31 +0100

    improvements to indexing

--- metric-platform/metric-providers/org.eclipse.scava.metricprovider.indexing.bugs/src/org/eclipse/scava/metricprovider/indexing/bugs/mapping/Mapping.java
+++ metric-platform/metric-providers/org.eclipse.scava.metricprovider.indexing.bugs/src/org/eclipse/scava/metricprovider/indexing/bugs/mapping/Mapping.java
@@ -2,9 +2,75 @@ package org.eclipse.scava.metricprovider.indexing.bugs.mapping;
 
 public final class Mapping {
 	
-	private static final String bugMapping = "";
+	private static final String bugMapping = "{\n" + 
+			"  \"properties\" : {\n" + 
+			"    \"created_at\" : {\n" + 
+			"      \"type\" : \"date\"\n" + 
+			"    },\n" + 
+			"    \"bug_summary\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"severity\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"bug_id\" : {\n" + 
+			"      \"type\" : \"keyword\"\n" + 
+			"    },\n" + 
+			"    \"project_name\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"creator\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"uid\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    }\n" + 
+			"  }\n" + 
+			"}";
 	
-	private static final String commentMapping = "";
+	private static final String commentMapping = "{\n" + 
+			"  \"properties\" : {\n" + 
+			"    \"comment_Id\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"body\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"emotional_dimension\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"sentiment\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"plain_text\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"request_reply_classification\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"content_class\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"contains_code\" : {\n" + 
+			"      \"type\" : \"boolean\"\n" + 
+			"    },\n" + 
+			"    \"bug_id\" : {\n" + 
+			"      \"type\" : \"keyword\"\n" + 
+			"    },\n" + 
+			"    \"project_name\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"creator\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    },\n" + 
+			"    \"created_at\" : {\n" + 
+			"      \"type\" : \"date\"\n" + 
+			"    },\n" + 
+			"    \"uid\" : {\n" + 
+			"      \"type\" : \"text\"\n" + 
+			"    }\n" + 
+			"  }\n" + 
+			"}";
 	
 	//=======================================================================
 	
