e08963a870466c67f234cfdb6f7c1ab44c0ff813
D20 <danielcampbell2097@hotmail.com>
2019-05-20 17:34:04 +0100

    Indexer RestClient modifications

--- metric-platform/metric-providers/org.eclipse.scava.index.indexer/src/org/eclipse/scava/index/indexer/IndexerSingleton.java
+++ metric-platform/metric-providers/org.eclipse.scava.index.indexer/src/org/eclipse/scava/index/indexer/IndexerSingleton.java
@@ -4,8 +4,14 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
@@ -83,10 +89,26 @@ class IndexerSingleton {
 	 */
 	private RestHighLevelClient createHighLevelClient() {
 
-		RestHighLevelClient client = new RestHighLevelClient(
-				RestClient.builder(new HttpHost(hostname, port, scheme),
-						new HttpHost(hostname, port + 1, scheme)));
-		return client;
+	
+		final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+		credentialsProvider.setCredentials(AuthScope.ANY,
+		        new UsernamePasswordCredentials("admin", "admin"));
+
+
+		RestClientBuilder builder = RestClient.builder(new HttpHost("localhost", 9200), new HttpHost(hostname, port+1, scheme))
+		        .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+		            @Override
+		            public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+		                return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+		            }
+
+		        });
+		
+		
+		RestHighLevelClient highLevelClient = new RestHighLevelClient(builder);
+	
+				
+		return  highLevelClient;
 
 	}
 	/**
