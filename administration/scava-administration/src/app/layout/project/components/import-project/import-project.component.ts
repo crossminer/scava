f3001124eb10445f0c0b55a0fde4bf9e0f0a2d34
ambpro <amin.boudeffa@softeam.fr>
2019-05-27 14:59:38 +0200

    Check github/gitlab token properties existing admin-ui

--- administration/scava-administration/src/app/layout/project/components/import-project/import-project.component.ts
+++ administration/scava-administration/src/app/layout/project/components/import-project/import-project.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Project, IProject } from '../../project.model';
 import { ImportProjectService } from '../../../../shared/services/project-service/import-project.service';
 import { Router } from '@angular/router';
+import { PropertiesService } from '../../../../shared/services/properties-service/properties.service';
+import { Properties } from '../../../properties/properties.model';
 
 @Component({
     selector: 'app-import-project',
@@ -12,10 +14,13 @@ export class ImportProjectComponent implements OnInit {
 
     project: Project;
     isSaving: boolean;
+    githubTokenFount: boolean;
+    gitlabTokenFount: boolean;
 
     constructor(
         private router: Router,
-        private importProjectService: ImportProjectService
+        private importProjectService: ImportProjectService,
+        public propertiesService: PropertiesService,
     ) { }
 
     ngOnInit() {
@@ -25,12 +30,50 @@ export class ImportProjectComponent implements OnInit {
 
     save() {
         this.isSaving = true;
-        this.importProjectService.importProject(this.project).subscribe(resp => {
-            this.router.navigate(['/project']);
-        }, (error) => {
-            this.isSaving = false;
-            this.onShowMessage(error);
-        });
+        if (this.githubTokenFount || this.gitlabTokenFount) {
+            this.importProjectService.importProject(this.project).subscribe(resp => {
+                this.router.navigate(['/project']);
+            }, (error) => {
+                this.isSaving = false;
+                this.onShowMessage(error);
+            });
+        }
+    }
+
+    checkPropertyExist(){
+        this.githubTokenFount = true;
+        this.gitlabTokenFount = true;
+        this.propertiesService.getProperties().subscribe(
+            (resp) => {
+                let properties: Properties[] = resp as Properties[];
+                if (this.project.url.startsWith("https://github")) {
+                    this.githubTokenFount = false;
+                    for (let property of properties) {
+                        if (property.key == "githubToken") {
+                            this.githubTokenFount = true;
+                            break;
+                        }
+                    };
+                    if (!this.githubTokenFount) {
+                        console.log("github token not-fount");
+                    }
+                } else if (this.project.url.startsWith("https://gitlab")) {
+                    this.gitlabTokenFount = false;
+                    for (let property of properties) {
+                        if (property.key == "gitlabToken") {
+                            this.gitlabTokenFount = true;
+                            break;
+                        }
+                    };
+                    if (!this.gitlabTokenFount) {
+                        console.log("gitlab token not-fount");
+                    }
+                }
+
+            },
+            (error) => {
+                this.onShowMessage(error);
+            });
     }
 
     previousState() {
