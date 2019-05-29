2a55efc273e36ef549cb5e3f88c1f04ab182051c
md2manoppello <juri.di.rocco@gmail.com>
2019-05-29 09:46:51 -0400

    the release date info is added  to version recommender

--- knowledge-base/org.eclipse.scava.knowledgebase/src/main/java/org/eclipse/scava/business/dto/RecommendedLibrary.java
+++ knowledge-base/org.eclipse.scava.knowledgebase/src/main/java/org/eclipse/scava/business/dto/RecommendedLibrary.java
@@ -1,9 +1,17 @@
 package org.eclipse.scava.business.dto;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class RecommendedLibrary {
 
 	private String libraryName;
 	private String url;
+
+    @JsonFormat
+    (shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+	private Date releaseDate;
 	public String getUrl() {
 		return url;
 	}
@@ -15,5 +23,11 @@ public class RecommendedLibrary {
 	}
 	public void setLibraryName(String libraryName) {
 		this.libraryName = libraryName;
+	}
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
 	}
 }
