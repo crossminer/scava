5ef02faa25df92610db21920259212074bf7a186
Luis Adrian Cabrera Diego <adrian89_@hotmail.com>
2019-05-30 14:17:44 +0100

    Factoids: Add support to new communicatons channels

--- metric-platform/factoids/org.eclipse.scava.factoid.newsgroups.hourly/src/org/eclipse/scava/factoid/newsgroups/hourly/NewsgroupsChannelHourlyFactoid.java
+++ metric-platform/factoids/org.eclipse.scava.factoid.newsgroups.hourly/src/org/eclipse/scava/factoid/newsgroups/hourly/NewsgroupsChannelHourlyFactoid.java
@@ -22,7 +22,11 @@ import org.eclipse.scava.platform.factoids.Factoid;
 import org.eclipse.scava.platform.factoids.StarRating;
 import org.eclipse.scava.repository.model.CommunicationChannel;
 import org.eclipse.scava.repository.model.Project;
+import org.eclipse.scava.repository.model.cc.eclipseforums.EclipseForum;
+import org.eclipse.scava.repository.model.cc.irc.Irc;
 import org.eclipse.scava.repository.model.cc.nntp.NntpNewsGroup;
+import org.eclipse.scava.repository.model.cc.sympa.SympaMailingList;
+import org.eclipse.scava.repository.model.sourceforge.Discussion;
 
 public class NewsgroupsChannelHourlyFactoid extends AbstractFactoidMetricProvider{
 
@@ -48,6 +52,10 @@ public class NewsgroupsChannelHourlyFactoid extends AbstractFactoidMetricProvide
 	public boolean appliesTo(Project project) {
 		for (CommunicationChannel communicationChannel: project.getCommunicationChannels()) {
 			if (communicationChannel instanceof NntpNewsGroup) return true;
+			if (communicationChannel instanceof Discussion) return true;
+			if (communicationChannel instanceof EclipseForum) return true;
+			if (communicationChannel instanceof SympaMailingList) return true;
+			if (communicationChannel instanceof Irc) return true;
 		}
 		return false;
 	}
