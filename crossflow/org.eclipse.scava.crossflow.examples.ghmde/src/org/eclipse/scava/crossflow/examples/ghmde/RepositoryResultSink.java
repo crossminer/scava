bec8f5c20e01f64c0a23387445f0f27a3f3ba74c
kb634 <kb634@york.ac.uk>
2019-06-12 14:46:14 +0100

    updated ghmde example

--- crossflow/org.eclipse.scava.crossflow.examples.ghmde/src/org/eclipse/scava/crossflow/examples/ghmde/RepositoryResultSink.java
+++ crossflow/org.eclipse.scava.crossflow.examples.ghmde/src/org/eclipse/scava/crossflow/examples/ghmde/RepositoryResultSink.java
@@ -17,15 +17,15 @@ import org.eclipse.scava.crossflow.runtime.utils.CrossflowLogger.SEVERITY;
 import org.eclipse.scava.crossflow.runtime.utils.CsvWriter;
 
 public class RepositoryResultSink extends RepositoryResultSinkBase {
-	
+
 	protected HashMap<String, AnalysisResult> results = new HashMap<String, AnalysisResult>();
-	
+
 	private boolean started = false;
 	private Timer t = new Timer();
 
 	@Override
 	public void consumeRepositoryResults(AnalysisResult analysisResult) throws Exception {
-		System.out.println("sink consuming: "+analysisResult);
+		System.out.println("sink consuming: " + analysisResult);
 
 		if (!started)
 			t.schedule(new TimerTask() {
@@ -35,20 +35,20 @@ public class RepositoryResultSink extends RepositoryResultSinkBase {
 				}
 			}, 2000, 2000);
 		started = true;
-		
-		if ( !results.containsKey(analysisResult.getName()) && analysisResult.getName()!=null ) {
+
+		if (!results.containsKey(analysisResult.getName()) && analysisResult.getName() != null) {
 			// add new item
 			results.put(analysisResult.getName(), analysisResult);
-			
-		} else if ( results.containsKey(analysisResult.getName()) ) {
+
+		} else if (results.containsKey(analysisResult.getName())) {
 			// supplement new item with existing information (if available)
 			AnalysisResult existingResult = results.get(analysisResult.getName());
 
-			if ( analysisResult.getAuthorCount()==-1 && existingResult.getAuthorCount()!=-1 ) {
-				analysisResult.setAuthorCount( existingResult.getAuthorCount() );
+			if (analysisResult.getAuthorCount() == -1 && existingResult.getAuthorCount() != -1) {
+				analysisResult.setAuthorCount(existingResult.getAuthorCount());
 			}
-			if ( analysisResult.getFileCount()==-1 && existingResult.getFileCount()!=-1 ) {
-				analysisResult.setFileCount( existingResult.getFileCount() );
+			if (analysisResult.getFileCount() == -1 && existingResult.getFileCount() != -1) {
+				analysisResult.setFileCount(existingResult.getFileCount());
 			}
 			results.replace(existingResult.getName(), analysisResult);
 		}
@@ -67,12 +67,14 @@ public class RepositoryResultSink extends RepositoryResultSinkBase {
 			File output = new File(workflow.getOutputDirectory(), "output.csv");
 			File outputTemp = new File(workflow.getOutputDirectory(), "output-temp.csv");
 
-			CsvWriter writer = new CsvWriter(outputTemp.getAbsolutePath(), "repository name", "MDE technology file count", "unique author count", "cached");
-			
-			for ( AnalysisResult result : results.values() ) {
-				writer.writeRecord(result.getName(), result.getFileCount(), result.getAuthorCount(), result.isCached());
+			CsvWriter writer = new CsvWriter(outputTemp.getAbsolutePath(), "repository name", "MDE technology",
+					"file count", "unique author count", "cached");
+
+			for (AnalysisResult result : results.values()) {
+				writer.writeRecord(result.getName(), result.getTechnology(), result.getFileCount(),
+						result.getAuthorCount(), result.isCached());
 			}
-			
+
 			writer.flush();
 			writer.close();
 
@@ -81,10 +83,10 @@ public class RepositoryResultSink extends RepositoryResultSinkBase {
 			outputTemp.renameTo(new File(workflow.getOutputDirectory(), "output.csv"));
 
 		} catch (Exception ex) {
-			log(SEVERITY.ERROR, "Exception occurred while flushing workflow output to disk. Message: " + ex.getMessage());
+			log(SEVERITY.ERROR,
+					"Exception occurred while flushing workflow output to disk. Message: " + ex.getMessage());
 		}
 	}
 	// ----
-	
 
 }
