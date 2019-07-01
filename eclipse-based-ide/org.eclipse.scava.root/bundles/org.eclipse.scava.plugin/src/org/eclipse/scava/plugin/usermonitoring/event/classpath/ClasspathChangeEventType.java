c3bb73aed9f3ea980528f1ceb42e7c73a9330cae
Rudolf Ferenc <ferenc@frontendart.com>
2019-07-01 08:59:55 +0200

    removing old code

--- eclipse-based-ide/org.eclipse.scava.root/bundles/org.eclipse.scava.plugin/src/org/eclipse/scava/plugin/usermonitoring/event/classpath/ClasspathChangeEventType.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.scava.plugin.usermonitoring.event.classpath;
-
-public enum ClasspathChangeEventType {
-	ADDED, DELETED, SCAVA_ADDED, SCAVA_DELETED
-}
--- eclipse-based-ide/org.eclipse.scava.root/bundles/org.eclipse.scava.plugin/src/org/eclipse/scava/plugin/usermonitoring/metric/term/ITerm.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.scava.plugin.usermonitoring.metric.term;
-
-public interface ITerm {
-
-	public int getCount();
-}
