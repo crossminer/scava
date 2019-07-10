26a501ffafded9dbd6a88165ae31560c86ad45e0
Jon Co <jonathanco1991@gmail.com>
2019-07-10 15:28:49 +0100

    Updated Wordcount example

--- crossflow/org.eclipse.scava.crossflow.examples.wordcount/py/main.py
+++ crossflow/org.eclipse.scava.crossflow.examples.wordcount/py/main.py
@@ -2,17 +2,17 @@ import argparse
 import sys
 import time
 
-from org.eclipse.scava.crossflow.runtime import Mode
-from org.eclipse.scava.crossflow.examples.wordcount.runtime.WordCountWorkflow import WordCountWorkflow
+from crossflow.wordcount.word_count_workflow import WordCountWorkflow
+from crossflow.runtime import Mode
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-name', default='Word Count Example', help='The name of the workflow')
+	parser.add_argument('-name', default='WordCountWorkflowPython', help='The name of the workflow')
 	parser.add_argument('-master', default='localhost', help='IP of the master')
 	parser.add_argument('-brokerHost', default='localhost', help='IP of the broker')
 	parser.add_argument('-stomp', default=61613, help='Port to use for stomp based messages')
-	parser.add_argument('-instance', default='Word Count Example', help='The instance of the master (to contribute to)')
+	parser.add_argument('-instance', default=None, help='The instance of the master (to contribute to)')
 	parser.add_argument('-mode', default=Mode.WORKER, help='Must be master_bare, master or worker')
 	
 	parsedArgs = parser.parse_args(sys.argv[1:len(sys.argv)])
