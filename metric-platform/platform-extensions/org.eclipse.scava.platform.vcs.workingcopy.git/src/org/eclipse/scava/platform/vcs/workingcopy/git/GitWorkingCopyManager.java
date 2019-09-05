3a93b2b7f3141b37fca4c50c42c493c57699a595
tdegueul <degueule@cwi.nl>
2019-09-05 11:26:17 +0200

    [GitWorkingCopyManager] Kill and retry 'git pull' operations if they do not complete in 5 minutes

--- metric-platform/platform-extensions/org.eclipse.scava.platform.vcs.workingcopy.git/src/org/eclipse/scava/platform/vcs/workingcopy/git/GitWorkingCopyManager.java
+++ metric-platform/platform-extensions/org.eclipse.scava.platform.vcs.workingcopy.git/src/org/eclipse/scava/platform/vcs/workingcopy/git/GitWorkingCopyManager.java
@@ -13,10 +13,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
+import org.eclipse.scava.platform.logging.OssmeterLogger;
 import org.eclipse.scava.platform.vcs.workingcopy.manager.Churn;
 import org.eclipse.scava.platform.vcs.workingcopy.manager.WorkingCopyCheckoutException;
 import org.eclipse.scava.platform.vcs.workingcopy.manager.WorkingCopyManager;
@@ -24,6 +29,7 @@ import org.eclipse.scava.repository.model.VcsRepository;
 import org.eclipse.scava.repository.model.vcs.git.GitRepository;
 
 public class GitWorkingCopyManager implements WorkingCopyManager {
+  private Logger loggerOssmeter = OssmeterLogger.getLogger("GitWorkingCopyManager");
 
   public GitWorkingCopyManager() {
     // DO NOT REMOVE
@@ -38,10 +44,16 @@ public class GitWorkingCopyManager implements WorkingCopyManager {
   public synchronized void checkout(File workingDirectory, VcsRepository repository, String revision)
       throws WorkingCopyCheckoutException {
     try {
+      loggerOssmeter.info("Checkout " + repository.getUrl() + " in revision " + revision);
+      Instant start = Instant.now();
       if (workingDirectory.exists()) {
         // we assume we did it before and will now just pull
         Process p = Runtime.getRuntime().exec(new String[] { "git", "pull"}, new String[] { }, workingDirectory);
-        p.waitFor();
+        if (!p.waitFor(5, TimeUnit.MINUTES)) {
+          loggerOssmeter.error("Couldn't complete git pull in 5 minutes. Killing and retrying...");
+          p.destroy();
+          checkout(workingDirectory, repository, revision);
+        }
       }
       else {
         // we clone
@@ -50,6 +62,8 @@ public class GitWorkingCopyManager implements WorkingCopyManager {
       }
       Process p = Runtime.getRuntime().exec(new String[] {"git", "checkout", revision }, null, workingDirectory);
       p.waitFor();
+      Instant end = Instant.now();
+      loggerOssmeter.info("Checkout completed in " + Duration.between(start, end).getSeconds() + " seconds.");
     } catch (IOException | InterruptedException e) {
       throw new WorkingCopyCheckoutException(repository, revision, e);
     }
