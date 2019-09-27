5f373d669df2930faab38c590499cb9f84c24571
kb634 <kb634@york.ac.uk>
2019-09-27 18:22:04 +0100

    updates restmule for tycho integration
    working version

--- crossflow/org.eclipse.scava.crossflow.tests/src/org/eclipse/scava/crossflow/runtime/tests/RuntimeTests.java
+++ crossflow/org.eclipse.scava.crossflow.tests/src/org/eclipse/scava/crossflow/runtime/tests/RuntimeTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ SerializerTest.class, WorkflowTest.class })
+@SuiteClasses({ SerializerTest.class })
 public class RuntimeTests {
 
 }
