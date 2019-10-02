1a3523090f039907c7a35eb92a2a3c9ade0f3c39
md2manoppello <juri.di.rocco@gmail.com>
2019-10-02 17:14:00 +0200

    [KB] Ossmeter importer: minor changes

--- knowledge-base/org.eclipse.scava.knowledgebase/src/main/java/org/eclipse/scava/business/impl/OssmeterImporter.java
+++ knowledge-base/org.eclipse.scava.knowledgebase/src/main/java/org/eclipse/scava/business/impl/OssmeterImporter.java
@@ -57,6 +57,7 @@ public class OssmeterImporter implements IImporter {
 
 	@Value("${ossmeter.url}")
 	private String ossmeterUrl;
+	private String ossmeterUrl2;
 
 	@Autowired
 	private MavenLibraryRepository mavenLibraryRepository;
@@ -82,27 +83,27 @@ public class OssmeterImporter implements IImporter {
 			result = new Artifact();
 		else
 			logger.info("Updating {} metadata", projectName);
-		URL url = new URL(ossmeterUrl + "projects/p/" + projectName);
+		URL url = new URL(ossmeterUrl2 + "projects/p/" + projectName);
 		URLConnection connection = url.openConnection();
 		connection.connect();
 		InputStream is = connection.getInputStream();
 		BufferedReader bufferReader = new BufferedReader(new InputStreamReader(is, Charset.forName(UTF8)));
 		String jsonText = readAll(bufferReader);
 		JSONObject obj = (JSONObject) JSONValue.parse(jsonText);
-		result.setClone_url((String) obj.get("clone_url"));
-		result.setFullName((String) obj.get("full_name"));
-		result.setFork((boolean) obj.get("fork"));
-		result.setGit_url((String) obj.get("git_url"));
-		result.setHtml_url((String) obj.get("html_url"));
-		result.setName((String) obj.get("name"));
-		result.setPrivate_((boolean) obj.get("_private"));
-		result.setShortName((String) obj.get("shortName"));
-		result.setSize((long) obj.get("size"));
-		result.setSsh_url((String) obj.get("ssh_url"));
-		result.setSvn_url((String) obj.get("svn_url"));
-		result.setMetricPlatformId((String) obj.get("shortName"));
-		result.setDescription((String) obj.get("description"));
-		result.setReadmeText((String) obj.get("description"));
+		if (obj.get("clone_url") != null) result.setClone_url((String) obj.get("clone_url"));
+		if (obj.get("full_name") != null) result.setFullName((String) obj.get("full_name"));
+		if (obj.get("fork") != null) result.setFork((boolean) obj.get("fork"));
+		if (obj.get("git_url") != null) result.setGit_url((String) obj.get("git_url"));
+		if (obj.get("html_url") != null) result.setHtml_url((String) obj.get("html_url"));
+		if (obj.get("name") != null) result.setName((String) obj.get("name"));
+		if (obj.get("_private") != null) result.setPrivate_((boolean) obj.get("_private"));
+		if (obj.get("shortName") != null) result.setShortName((String) obj.get("shortName"));
+		if (obj.get("size") != null) result.setSize((long) obj.get("size"));
+		if (obj.get("ssh_url") != null) result.setSsh_url((String) obj.get("ssh_url"));
+		if (obj.get("svn_url") != null) result.setSvn_url((String) obj.get("svn_url"));
+		if (obj.get("shortName") != null) result.setMetricPlatformId((String) obj.get("shortName"));
+		if (obj.get("description") != null) result.setDescription((String) obj.get("description"));
+		if (obj.get("descritpion") != null) result.setReadmeText((String) obj.get("description"));
 		is.close();
 		bufferReader.close();
 		result.setStarred(getStargazers(projectName));
@@ -121,7 +122,7 @@ public class OssmeterImporter implements IImporter {
 		List<Stargazers> result = new ArrayList<>();
 		try {
 
-			URL url = new URL(ossmeterUrl + "raw/projects/p/" + artId + "/m/stars");
+			URL url = new URL(ossmeterUrl2 + "raw/projects/p/" + artId + "/m/stars");
 			URLConnection connection = url.openConnection();
 			InputStream is = connection.getInputStream();
 			BufferedReader bufferReader = new BufferedReader(new InputStreamReader(is, Charset.forName(UTF8)));
@@ -147,7 +148,7 @@ public class OssmeterImporter implements IImporter {
 		List<String> result = new ArrayList<>();
 		try {
 			URL url = new URL(
-					ossmeterUrl + "raw/projects/p/" + artId + "/m/trans.rascal.dependency.maven.allMavenDependencies");
+					ossmeterUrl2 + "raw/projects/p/" + artId + "/m/trans.rascal.dependency.maven.allMavenDependencies");
 			URLConnection connection = url.openConnection();
 			InputStream is = connection.getInputStream();
 			BufferedReader bufferReader = new BufferedReader(new InputStreamReader(is, Charset.forName(UTF8)));
@@ -182,9 +183,9 @@ public class OssmeterImporter implements IImporter {
 		while (guard) {
 			try {
 				if(!mppUrl.startsWith("http://"))
-					ossmeterUrl = "http://" + mppUrl + "/";
-				else ossmeterUrl = mppUrl + "/";
-				URL url = new URL(ossmeterUrl + "projects/?page=" + page + "&size=" + pageSize);
+					ossmeterUrl2 = "http://" + mppUrl + "/";
+				else ossmeterUrl2 = mppUrl + "/";
+				URL url = new URL(ossmeterUrl2 + "projects/?page=" + page + "&size=" + pageSize);
 				URLConnection connection = url.openConnection();
 				connection.connect();
 				InputStream is = connection.getInputStream();
