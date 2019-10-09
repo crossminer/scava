45580248e7617001a1014e8645672dfd1f949ad2
Luis Adrian Cabrera Diego <adrian89_@hotmail.com>
2019-10-09 18:37:43 +0100

    Fix wrong user field in Mboxes

--- metric-platform/platform-extensions/org.eclipse.scava.platform.communicationchannel.mbox/src/org/eclipse/scava/platform/communicationchannel/mbox/MboxEmail.java
+++ metric-platform/platform-extensions/org.eclipse.scava.platform.communicationchannel.mbox/src/org/eclipse/scava/platform/communicationchannel/mbox/MboxEmail.java
@@ -11,28 +11,20 @@ public class MboxEmail extends CommunicationChannelArticle{
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private String sender;
 	private String recipient;
 	private String replyTo;
 	
 	public MboxEmail (Email email, Mbox mbox) {
 		this.setCommunicationChannel(mbox);;
 		this.setDate(email.getDate());
-		this.setSender(email.getFrom());
+		this.setUser(email.getFrom());
 		this.setArticleId(email.getMessageId());
 		this.setReferences(email.getReferences());
-		this.setReplyTo(email.getReplyTo());
+		this.replyTo=email.getReplyTo();
 		this.setSubject(email.getSubject());
 		this.setText(email.getText());
-		this.setRecipient(email.getTo());
+		this.recipient=email.getTo();
 		this.setArticleNumber(this.getDate().getTime());
-	}
-
-	/**
-	 * @return the sender
-	 */
-	public String getSender() {
-		return sender;
 	}
 
 	/**
@@ -43,32 +35,12 @@ public class MboxEmail extends CommunicationChannelArticle{
 	}
 
 	/**
-	 * @param sender the sender to set
-	 */
-	public void setSender(String sender) {
-		this.sender = sender;
-	}
-
-	/**
-	 * @param recipient the recipient to set
-	 */
-	public void setRecipient(String recipient) {
-		this.recipient = recipient;
-	}
-
-	/**
 	 * @return the replyTo
 	 */
 	public String getReplyTo() {
 		return replyTo;
 	}
 
-	/**
-	 * @param replyTo the replyTo to set
-	 */
-	public void setReplyTo(String replyTo) {
-		this.replyTo = replyTo;
-	}
 	
 	
 
