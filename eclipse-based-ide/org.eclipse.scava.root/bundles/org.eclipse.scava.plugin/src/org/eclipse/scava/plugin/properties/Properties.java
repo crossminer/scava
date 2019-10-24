4cb91a81951f5a9045414005d3380932a71395ae
Idarav <idaravikusz@gmail.com>
2019-10-24 16:00:12 +0200

    remove old source

--- eclipse-based-ide/org.eclipse.scava.root/bundles/org.eclipse.scava.plugin/src/org/eclipse/scava/plugin/properties/Properties.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.scava.plugin.properties;
-
-import org.eclipse.core.runtime.QualifiedName;
-import org.eclipse.scava.plugin.Activator;
-
-public class Properties {
-	public static final QualifiedName PROJECT_GITHUB_URL = new QualifiedName(Activator.PLUGIN_ID, "PROJECT_GITHUB_URL");
-}
