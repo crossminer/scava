895859d27a45af882d76bd5ca09ecaf29f91c09a
Jon Co <jonathanco1991@gmail.com>
2019-11-21 16:00:07 +0000

    Added missing test stub class

--- crossflow/org.eclipse.scava.crossflow.tests/src/typeGenerationWorkflow/TypeTask.java
+++ crossflow/org.eclipse.scava.crossflow.tests/src/typeGenerationWorkflow/TypeTask.java
@@ -3,6 +3,19 @@ package typeGenerationWorkflow;
 
 public class TypeTask extends TypeTaskBase {
 	
+	@Override
+	public GeneratedType consumeA(GeneratedType generatedType) throws Exception {
+		
+		GeneratedType generatedTypeInst = new GeneratedType();
+		//	generatedTypeInst.setStrProp( String );
+		//	generatedTypeInst.setIntProp( int );
+		//	generatedTypeInst.setManyStrProp( String );
+		//	generatedTypeInst.setManyIntProp( int );
+		//	generatedTypeInst.setEnumProp( org.eclipse.emf.ecore.impl.EClassImpl@7223f7fb (name: EnumField) (instanceClassName: null) (abstract: false, interface: false) );
+		//	generatedTypeInst.setManyEnumProp( org.eclipse.emf.ecore.impl.EClassImpl@7223f7fb (name: EnumField) (instanceClassName: null) (abstract: false, interface: false) );
+		return generatedTypeInst;
+	
+	}
 
 
 }
