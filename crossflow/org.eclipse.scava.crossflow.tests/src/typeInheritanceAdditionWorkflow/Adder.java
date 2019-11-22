8e18b67a885b3342d4414b3e7360c6f6576a3cdb
kb634 <kb634@york.ac.uk>
2019-11-22 12:59:12 +0000

    adds inheritance test for Type
    compilation-based test
    also minor update to type2class for capturing types as isJob() when one
    or more of the types extending them is a Job
    updates o.e.s.c.reusablecomponents example Types to ensure that their
    fields are not private, so that they can be accessed by their subclasses

---
+++ crossflow/org.eclipse.scava.crossflow.tests/src/typeInheritanceAdditionWorkflow/Adder.java
@@ -0,0 +1,16 @@
+package typeInheritanceAdditionWorkflow;
+
+
+public class Adder extends AdderBase {
+	
+	@Override
+	public Number consumeAdditions(NumberPair numberPair) throws Exception {
+		
+		Number numberInst = new Number();
+		//	numberInst.setN( int );
+		return numberInst;
+	
+	}
+
+
+}
