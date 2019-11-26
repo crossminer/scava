1cc92d3926184f8a280d78144f950f07a4d70d1f
Luis Adrian Cabrera Diego <adrian89_@hotmail.com>
2019-11-26 13:10:32 +0000

    Minor cleaning

--- metric-platform/metric-providers/org.eclipse.scava.metricprovider.trans.indexing.preparation/src/org/eclipse/scava/metricprovider/trans/indexing/preparation/IndexPreparationTransMetricProvider.java
+++ metric-platform/metric-providers/org.eclipse.scava.metricprovider.trans.indexing.preparation/src/org/eclipse/scava/metricprovider/trans/indexing/preparation/IndexPreparationTransMetricProvider.java
@@ -24,13 +24,11 @@ import org.eclipse.scava.platform.delta.communicationchannel.PlatformCommunicati
 import org.eclipse.scava.platform.delta.vcs.PlatformVcsManager;
 import org.eclipse.scava.repository.model.CommunicationChannel;
 import org.eclipse.scava.repository.model.Project;
-import org.eclipse.scava.repository.model.VcsRepository;
 import org.eclipse.scava.repository.model.cc.eclipseforums.EclipseForum;
 import org.eclipse.scava.repository.model.cc.irc.Irc;
 import org.eclipse.scava.repository.model.cc.mbox.Mbox;
 import org.eclipse.scava.repository.model.cc.nntp.NntpNewsGroup;
 import org.eclipse.scava.repository.model.cc.sympa.SympaMailingList;
-import org.eclipse.scava.repository.model.documentation.gitbased.DocumentationGitBased;
 import org.eclipse.scava.repository.model.documentation.systematic.DocumentationSystematic;
 import org.eclipse.scava.repository.model.sourceforge.Discussion;
 
