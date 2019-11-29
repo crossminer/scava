25a44f24fb0c3678fbaba47053224888230ddf6c
Luis Adrian Cabrera Diego <adrian89_@hotmail.com>
2019-11-29 12:04:12 +0000

    Update Sympa reader
    New logger
    
    Improved catching of errors
    
    Use local data storage

--- metric-platform/platform-extensions/org.eclipse.scava.platform.communicationchannel.sympa/src/org/eclipse/scava/platform/communicationchannel/sympa/SympaManager.java
+++ metric-platform/platform-extensions/org.eclipse.scava.platform.communicationchannel.sympa/src/org/eclipse/scava/platform/communicationchannel/sympa/SympaManager.java
@@ -8,9 +8,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.text.ParseException;
-import java.time.LocalDate;
-import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.eclipse.scava.platform.Date;
@@ -22,6 +21,10 @@ import org.eclipse.scava.platform.communicationchannel.sympa.utils.SympaUtils;
 import org.eclipse.scava.platform.delta.communicationchannel.CommunicationChannelArticle;
 import org.eclipse.scava.platform.delta.communicationchannel.CommunicationChannelDelta;
 import org.eclipse.scava.platform.delta.communicationchannel.ICommunicationChannelManager;
+import org.eclipse.scava.platform.logging.OssmeterLogger;
+import org.eclipse.scava.platform.storage.communicationchannel.CommunicationChannelData;
+import org.eclipse.scava.platform.storage.communicationchannel.CommunicationChannelDataManager;
+import org.eclipse.scava.platform.storage.communicationchannel.CommunicationChannelDataPath;
 import org.eclipse.scava.repository.model.CommunicationChannel;
 import org.eclipse.scava.repository.model.cc.sympa.SympaMailingList;
 
@@ -29,8 +32,12 @@ import com.mongodb.DB;
 
 public class SympaManager implements ICommunicationChannelManager<SympaMailingList> {
 
-	Path rootPath;
-	Boolean deltaFlag = false;
+	private static OssmeterLogger logger;
+	
+	static
+	{
+		logger = (OssmeterLogger) OssmeterLogger.getLogger("platform.communicationchannel.mbox");
+	}
 
 	@Override
 	public boolean appliesTo(CommunicationChannel communicationChannel) {
@@ -61,91 +68,83 @@ public class SympaManager implements ICommunicationChannelManager<SympaMailingLi
 
 	private List<Email> getMailingList(SympaMailingList sympa, Date date) {
 
-		LocalDate localDate = date.toJavaDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-
-		int year = localDate.getYear();
-		String month = SympaUtils.checkDateValue(localDate.getMonthValue());
-		String day = SympaUtils.checkDateValue(localDate.getDayOfMonth());
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date.toJavaDate());
+		
+		int year = calendar.get(Calendar.YEAR);
+		String month = SympaUtils.checkDateValue(calendar.get(Calendar.MONTH));
+		String day = SympaUtils.checkDateValue(calendar.get(Calendar.DAY_OF_MONTH));
 		String ext = SympaUtils.checkExtension(sympa.getCompressedFileExtension());
-
-		String downloadLink = "";
-
-		if (!sympa.getUrl().endsWith("/")) {
-			downloadLink = sympa.getUrl() + "/" + sympa.getMailingListName() + "-" + year + "-" + month + "-" + day
-					+ ext;
-
-		} else {
-
-			downloadLink = sympa.getUrl() + sympa.getMailingListName() + "-" + year + "-" + month + "-" + day + ext;
+		
+		if(ext.isEmpty())
+		{
+			logger.error("File extension is empty. Returning empty delta.");
+			return new ArrayList<Email>();
 		}
+		
+		CommunicationChannelData data = CommunicationChannelDataManager.getData(sympa.getOSSMeterId());
+		
+		CommunicationChannelDataPath dataPath;
+		
+		if(!data.compareDate(calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH), year) || !data.fileExists())
+		{
+			String downloadLink = sympa.getUrl();
+			if(!downloadLink.endsWith("/"))
+				downloadLink+="/";
+			
+			downloadLink+=sympa.getMailingListName() + "-" + year + "-" + month + "-" + day	+ ext;
 
-		if (sympa.getUsername().equals("null") || sympa.getPassword().equals("null")) {
-
-			downloadMailingList(downloadLink, ext);
-
-		} else {
-
-			downloadMailingList(downloadLink, sympa.getUsername(), sympa.getPassword(), ext);
-
+			if (sympa.getUsername().isEmpty() || sympa.getPassword().isEmpty())
+				dataPath=downloadMailingList(data.getTempDir(), downloadLink, ext);
+			else
+				dataPath=downloadMailingList(data.getTempDir(), downloadLink, sympa.getUsername(), sympa.getPassword(), ext);
+			if(dataPath!=null)
+			{
+				data.updateDataPath(dataPath, calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH), year);
+				CommunicationChannelDataManager.updateData(sympa.getOSSMeterId(), data);
+			}
 		}
-
-		if (deltaFlag == false) {
-
-			 
-			return SympaPlainTextFileReader.parseFolder(rootPath);
+		else
+			dataPath=data.getDataPath();
+		
+		if(dataPath==null)
+		{
+			logger.error("File not found. Returning empty delta.");
+			return new ArrayList<Email>();
 		}
-
-		return new ArrayList<Email>();
+	 
+		try {
+			return SympaPlainTextFileReader.parseFolder(dataPath.getPath());
+		} catch (IOException e) {
+			logger.error("Error whule parsing stored file. Returning empty delta.", e);
+			return new ArrayList<Email>();
+		}
 	}
 
 	/**
 	 * Download Data from URL using no authentication
 	 * 
 	 * @param downloadURL
+	 * @return 
 	 */
-	private void downloadMailingList(String downloadURL, String ext) {
+	private CommunicationChannelDataPath downloadMailingList(Path tempDir, String downloadURL, String ext) {
 
-		deltaFlag = false;
-		URL url = null;
-
+		CommunicationChannelDataPath dataPath=null;
 		try {
 
-			url = new URL(downloadURL);
+			URL url = new URL(downloadURL);
 			InputStream is = url.openStream();
-			rootPath = TgzExtractor.extract(is, ext);
+			dataPath = TgzExtractor.extract(tempDir, is, ext);
 			is.close();
 
 		} catch (MalformedURLException e) {
-
-			e.printStackTrace();
-
+			logger.error("Malformed URL:"+ downloadURL, e);
 		} catch (FileNotFoundException e) {
-
-			// Wait for specified tmie and try again
-			try {
-
-				url = new URL(downloadURL);
-				InputStream is = url.openStream();
-				rootPath = TgzExtractor.extract(is, ext);
-				is.close();
-
-			} catch (MalformedURLException e1) {
-
-				e.printStackTrace();
-
-			} catch (FileNotFoundException e1) {
-
-				deltaFlag = true;
-
-			} catch (IOException e1) {
-
-				e.printStackTrace();
-			}
-
+			logger.error("Error while reading a file", e);
 		} catch (IOException e) {
-
-			e.printStackTrace();
+			logger.error(e);
 		}
+		return dataPath;
 
 	}
 
@@ -156,50 +155,12 @@ public class SympaManager implements ICommunicationChannelManager<SympaMailingLi
 	 * @param username
 	 * @param password
 	 */
-	private void downloadMailingList(String downloadURL, String username, String password, String ext) {
+	private CommunicationChannelDataPath downloadMailingList(Path tempDir, String downloadURL, String username, String password, String ext) {
 
 		MyAuthenticator.setPasswordAuthentication(username, password);
 		Authenticator.setDefault(new MyAuthenticator());
-		deltaFlag = false;
-		URL url = null;
-
-		try {
-
-			url = new URL(downloadURL);
-			InputStream is = url.openStream();
-			rootPath = TgzExtractor.extract(is, ext);
-			is.close();
-
-		} catch (MalformedURLException e) {
-
-			e.printStackTrace();
-
-		} catch (FileNotFoundException e) {
-
-			// Wait for specified tmie and try again
-			try {
-
-				url = new URL(downloadURL);
-				InputStream is = url.openStream();
-				rootPath = TgzExtractor.extract(is, ext);
-				is.close();
-
-			} catch (MalformedURLException e1) {
-
-				e.printStackTrace();
-
-			} catch (FileNotFoundException e1) {
-				deltaFlag = true;
-				System.err.println("no archive found at :" + downloadURL);
-			} catch (IOException e1) {
-
-				e.printStackTrace();
-			}
-
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		}
+		
+		return downloadMailingList(tempDir, downloadURL, ext);
 
 	}
 
@@ -249,7 +210,7 @@ public class SympaManager implements ICommunicationChannelManager<SympaMailingLi
 		try {
 			firstDate = new Date(earliestDate.toString());
 		} catch (ParseException e) {
-			e.printStackTrace();
+			logger.error("Error while parsing a data: ", e);
 		}
 		return firstDate;
 	}
